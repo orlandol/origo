338ba15cc5eeebd46743f56e3b962fcdcc6b5528
Orlando Llanes <orlandol@veribox.net>
2019-04-16 02:52:13 -0400

    Initial upload.

---
+++ origo.c
@@ -0,0 +1,78 @@
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ *  Global variables
+ */
+  int argCount;
+  char** argVar;
+
+/*
+ *  Token declarations
+ */
+
+/*
+ *  Lexer declarations
+ */
+
+/*
+ *  Code generator declarations
+ */
+
+/*
+ *  Windows PE declarations
+ */
+
+/*
+ *  Backpatch declarations
+ */
+
+/*
+ *  Expression parser declarations
+ */
+
+/*
+ *  Parser declarations
+ */
+
+/*
+ *  Main program
+ */
+
+int main( int argc, char* argv[] )
+{
+  argCount = argc;
+  argVar = argv;
+
+  return 0;
+}
+
+/*
+ *  Token implementation
+ */
+
+/*
+ *  Lexer implementation
+ */
+
+/*
+ *  Code generator implementation
+ */
+
+/*
+ *  Windows PE implementation
+ */
+
+/*
+ *  Backpatch implementation
+ */
+
+/*
+ *  Expression parser implementation
+ */
+
+/*
+ *  Parser implementation
+ */
