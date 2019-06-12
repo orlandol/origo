910e73856adf2e293afb9654c0e352be3325a614
Orlando Llanes <orlandol@veribox.net>
2019-06-12 03:01:55 -0400

    Converted string and uint key array to single header file.

--- keyarray.h
+++ keyarray.h
@@ -2,7 +2,6 @@
 #define KEYARRAY_H
 
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 
 /*
@@ -76,7 +75,7 @@
   }
 
   #define DECLARE_STRING_KEYARRAY_INSERT( funcName, listType, dataType )\
-  bool funcName( listType* keyList, char* key, dataType* data ) {\
+  int funcName( listType* keyList, char* key, dataType* data ) {\
     unsigned leftIndex;\
     unsigned insertIndex;\
     unsigned rightIndex;\
@@ -89,12 +88,12 @@
     listType##Item* item;\
     \
     if( !(keyList && key && data) ) {\
-      return false;\
+      return 0;\
     }\
     \
     keyLen = strlen(key);\
     if( keyLen == 0 ) {\
-      return false;\
+      return 0;\
     }\
     \
     /* Grow list, if necessary */\
@@ -106,12 +105,12 @@
       prevCount = reservedCount;\
       reservedCount += 8;\
       if( prevCount > reservedCount ) {\
-        return false;\
+        return 0;\
       }\
       \
       item = realloc(item, reservedCount * sizeof(listType##Item));\
       if( item == NULL ) {\
-        return false;\
+        return 0;\
       }\
       keyList->reservedCount = reservedCount;\
       keyList->item = item;\
@@ -126,7 +125,7 @@
       result = strcmp(item[insertIndex].key, key);\
       \
       if( result == 0 ) {\
-        return false;\
+        return 0;\
       }\
       \
       if( result > 0 ) {\
@@ -141,7 +140,7 @@
     /* Attempt to allocate key string before going further */\
     newStrKey = malloc(keyLen + 1);\
     if( newStrKey == NULL ) {\
-      return false;\
+      return 0;\
     }\
     strcpy( newStrKey, key );\
     \
@@ -157,7 +156,7 @@
     \
     keyList->itemCount++;\
     \
-    return true;\
+    return 1;\
   }
 
   #define DECLARE_STRING_KEYARRAY_REMOVE( funcName, listType, freeDataFunc )\
@@ -218,7 +217,7 @@
   }
 
   #define DECLARE_STRING_KEYARRAY_RETRIEVE( funcName, listType, dataType )\
-  bool funcName( listType* keyList, char* key, dataType* destData ) {\
+  int funcName( listType* keyList, char* key, dataType* destData ) {\
     unsigned leftIndex;\
     unsigned rightIndex;\
     unsigned retrieveIndex;\
@@ -228,7 +227,7 @@
     listType##Item* item;\
     \
     if( !(keyList && keyList->item && key && (*key) && destData) ) {\
-      return false;\
+      return 0;\
     }\
     \
     reservedCount = keyList->reservedCount;\
@@ -245,7 +244,7 @@
       \
       if( result == 0 ) {\
         memcpy( destData, &(item[retrieveIndex].data), sizeof(dataType) );\
-        return true;\
+        return 1;\
       }\
       \
       if( result > 0 ) {\
@@ -257,7 +256,7 @@
       retrieveIndex = (leftIndex + rightIndex) / 2;\
     }\
     \
-    return false;\
+    return 0;\
   }
 
   #define DECLARE_STRING_KEYARRAY_RELEASEUNUSED( funcName, listType )\
@@ -371,5 +370,317 @@
  *  Unsigned Key Array implementation
  * ===================================
  */
+
+  #define DECLARE_UINT_KEYARRAY_TYPES(\
+      typeName, dataType )\
+  typedef struct typeName##Item {\
+    unsigned key;\
+    dataType data;\
+  } typeName##Item;\
+  \
+  typedef struct typeName {\
+    size_t reservedCount;\
+    size_t itemCount;\
+    typeName##Item* item;\
+  } typeName;
+
+  #define DECLARE_UINT_KEYARRAY_CREATE( funcName, listType )\
+  listType* funcName( size_t reserveCount ) {\
+    listType* newKeyArray = NULL;\
+    \
+    newKeyArray = (listType*)calloc(1, sizeof(listType));\
+    if( newKeyArray == NULL ) {\
+      goto ReturnError;\
+    }\
+    \
+    if( reserveCount ) {\
+      newKeyArray->item =\
+        (listType##Item*)calloc(reserveCount, sizeof(listType##Item));\
+      if( newKeyArray->item == NULL ) {\
+        goto ReturnError;\
+      }\
+      \
+      newKeyArray->reservedCount = reserveCount;\
+    }\
+    return newKeyArray;\
+    \
+  ReturnError:\
+    if( newKeyArray ) {\
+      if( newKeyArray->item ) {\
+        free( newKeyArray->item );\
+        newKeyArray->item = NULL;\
+      }\
+      free( newKeyArray );\
+      newKeyArray = NULL;\
+    }\
+    return NULL;\
+  }
+
+  #define DECLARE_UINT_KEYARRAY_FREE( funcName, listType, freeDataFunc )\
+  void funcName( listType** keyList ) {\
+    size_t index;\
+    size_t itemCount;\
+    \
+    if( keyList && (*keyList) ) {\
+      itemCount = (*keyList)->itemCount;\
+      for( index = 0; index < itemCount; index++ ) {\
+        freeDataFunc( &((*keyList)->item[index].data) );\
+      }\
+      \
+      free( (*keyList) );\
+      (*keyList) = NULL;\
+    }\
+  }
+
+  #define DECLARE_UINT_KEYARRAY_INSERT( funcName, listType, dataType )\
+  int funcName( listType* keyList,\
+      unsigned key, dataType* data ) {\
+    unsigned leftIndex;\
+    unsigned insertIndex;\
+    unsigned rightIndex;\
+    unsigned prevCount;\
+    unsigned reservedCount;\
+    unsigned itemCount;\
+    listType##Item* item;\
+    \
+    if( !(keyList && data) ) {\
+      return 0;\
+    }\
+    \
+    /* Grow list, if necessary */\
+    reservedCount = keyList->reservedCount;\
+    itemCount = keyList->itemCount;\
+    item = keyList->item;\
+    \
+    if( itemCount == reservedCount ) {\
+      prevCount = reservedCount;\
+      reservedCount += 8;\
+      if( prevCount > reservedCount ) {\
+        return 0;\
+      }\
+      \
+      item = realloc(item, reservedCount * sizeof(listType##Item));\
+      if( item == NULL ) {\
+        return 0;\
+      }\
+      keyList->reservedCount = reservedCount;\
+      keyList->item = item;\
+    }\
+    \
+    /* Search for insert position */\
+    leftIndex = 0;\
+    rightIndex = itemCount;\
+    insertIndex = itemCount / 2;\
+    \
+    while( leftIndex < rightIndex ) {\
+      if( item[insertIndex].key == key ) {\
+        return 0;\
+      }\
+      \
+      if( item[insertIndex].key > key ) {\
+        rightIndex = insertIndex;\
+      } else {\
+        leftIndex = insertIndex + 1;\
+      }\
+      \
+      insertIndex = (leftIndex + rightIndex) / 2;\
+    }\
+    \
+    /* Move data past insertion point up, if necessary */\
+    memmove( &(item[insertIndex + 1]), &(item[insertIndex]),\
+        (itemCount - insertIndex) * sizeof(listType##Item) );\
+    \
+    /* Insert item */\
+    item[insertIndex].key = key;\
+    if( data ) {\
+      memcpy( &(item[insertIndex].data), data, sizeof(dataType) );\
+    }\
+    \
+    keyList->itemCount++;\
+    \
+    return 1;\
+  }
+
+  #define DECLARE_UINT_KEYARRAY_REMOVE( funcName, listType, freeDataFunc )\
+  void funcName( listType* keyList, unsigned key ) {\
+    unsigned leftIndex;\
+    unsigned rightIndex;\
+    unsigned removeIndex;\
+    unsigned reservedCount;\
+    unsigned itemCount;\
+    listType##Item* item;\
+    \
+    if( !(keyList && keyList->item) ) {\
+      return;\
+    }\
+    \
+    reservedCount = keyList->reservedCount;\
+    itemCount = keyList->itemCount;\
+    item = keyList->item;\
+    \
+    /* Search for insert position */\
+    leftIndex = 0;\
+    rightIndex = itemCount;\
+    removeIndex = itemCount / 2;\
+    \
+    while( leftIndex < rightIndex ) {\
+      if( item[removeIndex].key == key ) {\
+        freeDataFunc( &(item[removeIndex].data) );\
+        \
+        if( itemCount ) {\
+          itemCount--;\
+          \
+          memcpy( &(item[removeIndex]), &(item[removeIndex + 1]),\
+            (itemCount - removeIndex) * sizeof(listType##Item) );\
+          \
+          keyList->itemCount = itemCount;\
+        }\
+        \
+        memset( &(item[itemCount]), 0, sizeof(listType##Item) );\
+        \
+        return;\
+      }\
+      \
+      if( item[removeIndex].key > key ) {\
+        rightIndex = removeIndex;\
+      } else {\
+        leftIndex = removeIndex + 1;\
+      }\
+      \
+      removeIndex = (leftIndex + rightIndex) / 2;\
+    }\
+  }
+
+  #define DECLARE_UINT_KEYARRAY_RETRIEVE( funcName, listType, dataType )\
+  int funcName( listType* keyList, unsigned key,\
+      dataType* destData ) {\
+    unsigned leftIndex;\
+    unsigned rightIndex;\
+    unsigned retrieveIndex;\
+    unsigned reservedCount;\
+    unsigned itemCount;\
+    listType##Item* item;\
+    \
+    if( !(keyList && keyList->item && destData) ) {\
+      return 0;\
+    }\
+    \
+    reservedCount = keyList->reservedCount;\
+    itemCount = keyList->itemCount;\
+    item = keyList->item;\
+    \
+    /* Search for insert position */\
+    leftIndex = 0;\
+    rightIndex = itemCount;\
+    retrieveIndex = itemCount / 2;\
+    \
+    while( leftIndex < rightIndex ) {\
+      if( item[retrieveIndex].key == key ) {\
+        memcpy( destData, &(item[retrieveIndex].data), sizeof(dataType) );\
+        return 1;\
+      }\
+      \
+      if( item[retrieveIndex].key > key ) {\
+        rightIndex = retrieveIndex;\
+      } else {\
+        leftIndex = retrieveIndex + 1;\
+      }\
+      \
+      retrieveIndex = (leftIndex + rightIndex) / 2;\
+    }\
+    \
+    return 0;\
+  }
+
+  #define DECLARE_UINT_KEYARRAY_RELEASEUNUSED( funcName, listType )\
+  void funcName( listType* keyList ) {\
+    listType##Item* item;\
+    \
+    if( keyList == NULL ) {\
+      return;\
+    }\
+    \
+    if( keyList->item && keyList->itemCount ) {\
+      /* Resize to remove reserved space */\
+      item = realloc(keyList->item,\
+        keyList->itemCount * sizeof(listType##Item));\
+      if( item ) {\
+        keyList->item = item;\
+        keyList->reservedCount = keyList->itemCount;\
+      }\
+    } else {\
+      /* Deallocate */\
+      keyList->reservedCount = 0;\
+      keyList->itemCount = 0;\
+      if( keyList->item ) {\
+        free( keyList->item );\
+        keyList->item = NULL;\
+      }\
+    }\
+  }
+
+  #define DECLARE_UINT_KEYARRAY_COPY( funcName, listType, dataType,\
+      copyDataFunc, freeDataFunc )\
+  listType* funcName( listType* sourceList ) {\
+    listType* newCopy = NULL;\
+    listType##Item* sourceItem = NULL;\
+    size_t reservedCount = 0;\
+    size_t itemCount = 0;\
+    size_t index;\
+    \
+    if( sourceList == NULL ) {\
+      return NULL;\
+    }\
+    \
+    /* Attempt to allocate list object */\
+    newCopy = calloc(1, sizeof(listType));\
+    if( newCopy == NULL ) {\
+      goto ReturnError;\
+    }\
+    \
+    /* Initialize important variables */\
+    reservedCount = sourceList->reservedCount;\
+    itemCount = sourceList->itemCount;\
+    sourceItem = sourceList->item;\
+    \
+    /* A list with no items is valid */\
+    if( !(reservedCount && itemCount && sourceItem) ) {\
+      return newCopy;\
+    }\
+    \
+    /* Copy data, then copy the Uint keys */\
+    newCopy->item = malloc(reservedCount * sizeof(listType##Item));\
+    if( newCopy->item == NULL ) {\
+      goto ReturnError;\
+    }\
+    \
+    for( index = 0; index < itemCount; index++ ) {\
+      copyDataFunc( &(newCopy->item[index].data),\
+          &(sourceItem[index].data) );\
+      \
+      newCopy->item[index].key = sourceItem[index].key;\
+    }\
+    \
+    newCopy->reservedCount = reservedCount;\
+    newCopy->itemCount = itemCount;\
+    \
+    return newCopy;\
+    \
+  ReturnError:\
+    if( newCopy == NULL ) {\
+      return NULL;\
+    }\
+    \
+    if( newCopy->item ) {\
+      for( index = 0; index < itemCount; index++ ) {\
+        freeDataFunc( &(newCopy->item[index].data) );\
+      }\
+    }\
+    \
+    free( newCopy );\
+    newCopy = NULL;\
+    \
+    return NULL;\
+  }
 
 #endif
--- testka.c
+++ testka.c
@@ -3,11 +3,13 @@
 
 #include "keyarray.h"
 
+/*
+ *  String Key Array declarations and tests.
+ */
+
   typedef struct FruitData {
     unsigned value;
   } FruitData;
-
-  DECLARE_STRING_KEYARRAY_TYPES( FruitList, FruitData )
 
   void CopyFruit( FruitData* destData, FruitData* sourceData ) {
     if( destData && sourceData ) {
@@ -18,6 +20,8 @@
   void FreeFruit( FruitData* data ) {
   }
 
+  DECLARE_STRING_KEYARRAY_TYPES( FruitList, FruitData )
+
   DECLARE_STRING_KEYARRAY_CREATE( CreateFruitList, FruitList )
   DECLARE_STRING_KEYARRAY_FREE( FreeFruitList, FruitList, FreeFruit )
   DECLARE_STRING_KEYARRAY_INSERT( InsertFruit, FruitList, FruitData )
@@ -26,128 +30,290 @@
   DECLARE_STRING_KEYARRAY_RELEASEUNUSED( ReleaseUnusedFruits, FruitList )
   DECLARE_STRING_KEYARRAY_COPY( CopyFruitList, FruitList, Fruit, CopyFruit, FreeFruit )
 
+  void RunStringKeyArrayTests( void ) {
+    FruitList* list;
+    FruitList* listCopy;
+    FruitData  data;
+
+    list = CreateFruitList(0);
+
+    printf( "\n" );
+    printf( "[Testing string key array]\n" );
+
+    data.value = 1234;
+    InsertFruit( list, "Orange", &data );
+
+    data.value = 2345;
+    InsertFruit( list, "Apple", &data );
+
+    data.value = 3456;
+    InsertFruit( list, "Zucchini", &data );
+
+    data.value = 4567;
+    InsertFruit( list, "Zucchini", &data );
+
+    data.value = 5678;
+    InsertFruit( list, "Apple", &data );
+
+    printf( "\n" );
+    printf( "Before Copy, and ReleaseUnused...\n" );
+    if( (list == NULL) || (list->item == NULL) ) {
+      printf( "  NULL list\n" );
+    } else {
+      for( unsigned i = 0; i < list->itemCount; i++ ) {
+        printf( "  key: %s; value: %u\n",
+          list->item[i].key, list->item[i].data.value );
+      }
+    }
+    printf( "\n" );
+
+    data.value = 0;
+    RetrieveFruit( list, "Apple", &data );
+    printf( "  Apple: %u\n", data.value );
+
+    data.value = 0;
+    RetrieveFruit( list, "Orange", &data );
+    printf( "  Orange: %u\n", data.value );
+
+    data.value = 0;
+    RetrieveFruit( list, "Zucchini", &data );
+    printf( "  Zucchini: %u\n", data.value );
+    printf( "\n" );
+
+    ReleaseUnusedFruits( list );
+
+    printf( "After ReleaseUnused...\n" );
+    if( (list == NULL) || (list->item == NULL) ) {
+      printf( "  NULL list\n" );
+    } else {
+      for( unsigned i = 0; i < list->itemCount; i++ ) {
+        printf( "  key: %s; value: %u\n",
+          list->item[i].key, list->item[i].data.value );
+      }
+    }
+    printf( "\n" );
+
+    data.value = 0;
+    RetrieveFruit( list, "Apple", &data );
+    printf( "  Apple: %u\n", data.value );
+
+    data.value = 0;
+    RetrieveFruit( list, "Orange", &data );
+    printf( "  Orange: %u\n", data.value );
+
+    data.value = 0;
+    RetrieveFruit( list, "Zucchini", &data );
+    printf( "  Zucchini: %u\n", data.value );
+
+    listCopy = CopyFruitList(list);
+
+    FreeFruitList( &list );
+
+    printf( "\n" );
+
+    printf( "After Copy...\n" );
+    if( (listCopy == NULL) || (listCopy->item == NULL) ) {
+      printf( "  NULL list\n" );
+    } else {
+      for( unsigned i = 0; i < listCopy->itemCount; i++ ) {
+        printf( "  key: %s; value: %u\n",
+          listCopy->item[i].key, listCopy->item[i].data.value );
+      }
+    }
+    printf( "\n" );
+
+    data.value = 0;
+    RetrieveFruit( listCopy, "Apple", &data );
+    printf( "  Apple: %u\n", data.value );
+
+    data.value = 0;
+    RetrieveFruit( listCopy, "Orange", &data );
+    printf( "  Orange: %u\n", data.value );
+
+    data.value = 0;
+    RetrieveFruit( listCopy, "Zucchini", &data );
+    printf( "  Zucchini: %u\n", data.value );
+
+    printf( "\n" );
+
+    printf( "After Remove...\n" );
+    RemoveFruit( listCopy, "Apple" );
+    RemoveFruit( listCopy, "Zucchini" );
+    RemoveFruit( listCopy, "Orange" );
+
+    if( (listCopy == NULL) || (listCopy->item == NULL) ) {
+      printf( "  NULL list\n" );
+    } else {
+      for( unsigned i = 0; i < listCopy->itemCount; i++ ) {
+        printf( "  key: %s; value: %u\n",
+          listCopy->item[i].key, listCopy->item[i].data.value );
+      }
+    }
+    printf( "\n" );
+
+    FreeFruitList( &listCopy );
+  }
+
+/*
+ *  Uint Key Array declarations and tests.
+ */
+
+  typedef struct FruitIDData {
+    unsigned value;
+  } FruitIDData;
+
+  void CopyFruitID( FruitIDData* destData, FruitIDData* sourceData ) {
+    if( destData && sourceData ) {
+      destData->value = sourceData->value;
+    }
+  }
+
+  void FreeFruitID( FruitIDData* data ) {
+  }
+
+  DECLARE_UINT_KEYARRAY_TYPES( FruitIDList, FruitIDData )
+
+  DECLARE_UINT_KEYARRAY_CREATE( CreateFruitIDList, FruitIDList )
+  DECLARE_UINT_KEYARRAY_FREE( FreeFruitIDList, FruitIDList, FreeFruit )
+  DECLARE_UINT_KEYARRAY_INSERT( InsertFruitID, FruitIDList, FruitIDData )
+  DECLARE_UINT_KEYARRAY_REMOVE( RemoveFruitID, FruitIDList, FreeFruit )
+  DECLARE_UINT_KEYARRAY_RETRIEVE( RetrieveFruitID, FruitIDList, FruitIDData )
+  DECLARE_UINT_KEYARRAY_RELEASEUNUSED( ReleaseUnusedFruitsID, FruitIDList )
+  DECLARE_UINT_KEYARRAY_COPY( CopyFruitListID, FruitIDList, Fruit, CopyFruit, FreeFruit )
+
+  void RunUintKeyArrayTests( void ) {
+    FruitIDList* list;
+    FruitIDList* listCopy;
+    FruitIDData data;
+  
+    list = CreateFruitIDList(0);
+  
+    printf( "\n" );
+    printf( "[Testing uint key array]\n" );
+
+    data.value = 1234;
+    InsertFruitID( list, 150, &data );
+  
+    data.value = 2345;
+    InsertFruitID( list, 100, &data );
+  
+    data.value = 3456;
+    InsertFruitID( list, 260, &data );
+  
+    data.value = 4567;
+    InsertFruitID( list, 260, &data );
+  
+    data.value = 5678;
+    InsertFruitID( list, 100, &data );
+  
+    printf( "\n" );
+    printf( "Before Copy, and ReleaseUnused...\n" );
+    if( (list == NULL) || (list->item == NULL) ) {
+      printf( "  NULL list\n" );
+    } else {
+      for( unsigned i = 0; i < list->itemCount; i++ ) {
+        printf( "  key: %u; value: %u\n",
+          list->item[i].key, list->item[i].data.value );
+      }
+    }
+    printf( "\n" );
+  
+    data.value = 0;
+    RetrieveFruitID( list, 100, &data );
+    printf( "  100: %u\n", data.value );
+  
+    data.value = 0;
+    RetrieveFruitID( list, 150, &data );
+    printf( "  150: %u\n", data.value );
+  
+    data.value = 0;
+    RetrieveFruitID( list, 260, &data );
+    printf( "  260: %u\n", data.value );
+    printf( "\n" );
+  
+    ReleaseUnusedFruitsID( list );
+  
+    printf( "After ReleaseUnused...\n" );
+    if( (list == NULL) || (list->item == NULL) ) {
+      printf( "  NULL list\n" );
+    } else {
+      for( unsigned i = 0; i < list->itemCount; i++ ) {
+        printf( "  key: %u; value: %u\n",
+          list->item[i].key, list->item[i].data.value );
+      }
+    }
+    printf( "\n" );
+  
+    data.value = 0;
+    RetrieveFruitID( list, 100, &data );
+    printf( "  100: %u\n", data.value );
+  
+    data.value = 0;
+    RetrieveFruitID( list, 150, &data );
+    printf( "  150: %u\n", data.value );
+  
+    data.value = 0;
+    RetrieveFruitID( list, 260, &data );
+    printf( "  260: %u\n", data.value );
+  
+    listCopy = CopyFruitListID(list);
+  
+    FreeFruitIDList( &list );
+  
+    printf( "\n" );
+  
+    printf( "After Copy...\n" );
+    if( (listCopy == NULL) || (listCopy->item == NULL) ) {
+      printf( "  NULL list\n" );
+    } else {
+      for( unsigned i = 0; i < listCopy->itemCount; i++ ) {
+        printf( "  key: %u; value: %u\n",
+          listCopy->item[i].key, listCopy->item[i].data.value );
+      }
+    }
+    printf( "\n" );
+  
+    data.value = 0;
+    RetrieveFruitID( listCopy, 100, &data );
+    printf( "  100: %u\n", data.value );
+  
+    data.value = 0;
+    RetrieveFruitID( listCopy, 150, &data );
+    printf( "  150: %u\n", data.value );
+  
+    data.value = 0;
+    RetrieveFruitID( listCopy, 260, &data );
+    printf( "  260: %u\n", data.value );
+  
+    printf( "\n" );
+  
+    printf( "After Remove...\n" );
+    RemoveFruitID( listCopy, 100 );
+    RemoveFruitID( listCopy, 260 );
+    RemoveFruitID( listCopy, 150 );
+  
+    if( (listCopy == NULL) || (listCopy->item == NULL) ) {
+      printf( "  NULL list\n" );
+    } else {
+      for( unsigned i = 0; i < listCopy->itemCount; i++ ) {
+        printf( "  key: %u; value: %u\n",
+          listCopy->item[i].key, listCopy->item[i].data.value );
+      }
+    }
+    printf( "\n" );
+
+    FreeFruitIDList( &listCopy );
+  }
+
+/*
+ *  Main program
+ */
+
 int main( int argc, char* argv[] ) {
-  FruitList* fruitList;
-  FruitList* fruitListCopy;
-  FruitData  data;
 
-  fruitList = CreateFruitList(0);
+  RunStringKeyArrayTests();
 
-  printf( "\n" );
-  printf( "Testing string key array.\n" );
-
-  data.value = 1234;
-  InsertFruit( fruitList, "Orange", &data );
-
-  data.value = 2345;
-  InsertFruit( fruitList, "Apple", &data );
-
-  data.value = 3456;
-  InsertFruit( fruitList, "Zucchini", &data );
-
-  data.value = 4567;
-  InsertFruit( fruitList, "Zucchini", &data );
-
-  data.value = 5678;
-  InsertFruit( fruitList, "Apple", &data );
-
-  printf( "\n" );
-  printf( "Before Copy, and ReleaseUnused...\n" );
-  if( (fruitList == NULL) || (fruitList->item == NULL) ) {
-    printf( "  NULL list\n" );
-  } else {
-    for( unsigned i = 0; i < fruitList->itemCount; i++ ) {
-      printf( "  key: %s; value: %u\n",
-        fruitList->item[i].key, fruitList->item[i].data.value );
-    }
-  }
-  printf( "\n" );
-
-  data.value = 0;
-  RetrieveFruit( fruitList, "Apple", &data );
-  printf( "  Apple: %u\n", data.value );
-
-  data.value = 0;
-  RetrieveFruit( fruitList, "Orange", &data );
-  printf( "  Orange: %u\n", data.value );
-
-  data.value = 0;
-  RetrieveFruit( fruitList, "Zucchini", &data );
-  printf( "  Zucchini: %u\n", data.value );
-  printf( "\n" );
-
-  ReleaseUnusedFruits( fruitList );
-
-  printf( "After ReleaseUnused...\n" );
-  if( (fruitList == NULL) || (fruitList->item == NULL) ) {
-    printf( "  NULL list\n" );
-  } else {
-    for( unsigned i = 0; i < fruitList->itemCount; i++ ) {
-      printf( "  key: %s; value: %u\n",
-        fruitList->item[i].key, fruitList->item[i].data.value );
-    }
-  }
-  printf( "\n" );
-
-  data.value = 0;
-  RetrieveFruit( fruitList, "Apple", &data );
-  printf( "  Apple: %u\n", data.value );
-
-  data.value = 0;
-  RetrieveFruit( fruitList, "Orange", &data );
-  printf( "  Orange: %u\n", data.value );
-
-  data.value = 0;
-  RetrieveFruit( fruitList, "Zucchini", &data );
-  printf( "  Zucchini: %u\n", data.value );
-
-  fruitListCopy = CopyFruitList(fruitList);
-
-  FreeFruitList( &fruitList );
-
-  printf( "\n" );
-
-  printf( "After Copy...\n" );
-  if( (fruitListCopy == NULL) || (fruitListCopy->item == NULL) ) {
-    printf( "  NULL list\n" );
-  } else {
-    for( unsigned i = 0; i < fruitListCopy->itemCount; i++ ) {
-      printf( "  key: %s; value: %u\n",
-        fruitListCopy->item[i].key, fruitListCopy->item[i].data.value );
-    }
-  }
-  printf( "\n" );
-
-  data.value = 0;
-  RetrieveFruit( fruitListCopy, "Apple", &data );
-  printf( "  Apple: %u\n", data.value );
-
-  data.value = 0;
-  RetrieveFruit( fruitListCopy, "Orange", &data );
-  printf( "  Orange: %u\n", data.value );
-
-  data.value = 0;
-  RetrieveFruit( fruitListCopy, "Zucchini", &data );
-  printf( "  Zucchini: %u\n", data.value );
-
-  printf( "\n" );
-
-  printf( "After Remove...\n" );
-  RemoveFruit( fruitListCopy, "Apple" );
-  RemoveFruit( fruitListCopy, "Zucchini" );
-  RemoveFruit( fruitListCopy, "Orange" );
-
-  if( (fruitListCopy == NULL) || (fruitListCopy->item == NULL) ) {
-    printf( "  NULL list\n" );
-  } else {
-    for( unsigned i = 0; i < fruitListCopy->itemCount; i++ ) {
-      printf( "  key: %s; value: %u\n",
-        fruitListCopy->item[i].key, fruitListCopy->item[i].data.value );
-    }
-  }
-  printf( "\n" );
-
-  FreeFruitList( &fruitList );
+  RunUintKeyArrayTests();
 
   return 0;
 }
