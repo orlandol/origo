b535382bcbee21bdfde28062e3e5bbeef40057b8
Orlando Llanes <orlandol@veribox.net>
2020-01-10 04:12:45 -0500

    Added some code.
    System console state save/restore in progress.
    App create/release in progress.

--- IDE/w32ui.c
+++ IDE/w32ui.c
@@ -7,7 +7,7 @@
 #include "Windows.h"
 
 /*
- * Console state implementation
+ * Console state declarations
  */
 
   typedef struct Win32ConsoleState {
@@ -16,23 +16,51 @@
     DWORD mode;
   } Win32ConsoleState;
 
-  Win32ConsoleState* SaveState( HANDLE consoleHandle ) {
+  Win32ConsoleState* AllocConsoleState();
+  void FreeConsoleState( Win32ConsoleState** consoleState );
+
+  bool SaveConsoleState( Win32ConsoleState* consoleState, HANDLE consoleHandle );
+  void RestoreConsoleState( Win32ConsoleState* consoleState );
+
+/*
+ * Console state implementation
+ */
+
+  Win32ConsoleState* AllocConsoleState() {
+    return calloc(1, sizeof(Win32ConsoleState));
+  }
+
+  void FreeConsoleState( Win32ConsoleState** consoleState ) {
+    if( consoleState ) {
+      if( (*consoleState) ) {
+        if( (*consoleState)->buffer ) {
+          CloseHandle( (*consoleState)->buffer );
+          (*consoleState)->buffer = NULL;
+        }
+
+        free( (*consoleState) );
+        (*consoleState) = NULL;
+      }
+    }
+  }
+
+  bool SaveConsoleState( Win32ConsoleState* consoleState,
+      HANDLE consoleHandle ) {
     return NULL;
   }
 
-  void FreeState( Win32ConsoleState** consoleState ) {
-  }
-
-  bool RestoreState( Win32ConsoleState* consoleState ) {
-    return false;
+  void RestoreConsoleState( Win32ConsoleState* consoleState ) {
   }
 
 /*
  * App internals
  */
   typedef struct Win32App {
-    int foo;
+    CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
+    HANDLE buffer;
   } Win32App;
+
+  Win32ConsoleState* oldStdOut = NULL;
 
 /*
  *  App implementation
@@ -42,12 +70,23 @@
 
     newApp = calloc(1, sizeof(Win32App));
 
+    ///TODO: Save system console state once
+
     return (App)newApp;
   }
 
   void FreeApp( App* app ) {
+    Win32App* w32App = NULL;
+
     if( app ) {
-      if( (*app) ) {
+      w32App = (*app);
+
+      if( w32App ) {
+        if( w32App->buffer ) {
+          CloseHandle( w32App->buffer );
+          w32App->buffer = NULL;
+        }
+
         free( (*app) );
         (*app) = NULL;
       }
@@ -55,4 +94,5 @@
   }
 
   void ExitApp( App app ) {
+    ///TODO: Restore system console state
   }
