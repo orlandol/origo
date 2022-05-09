918404aa636fa1ff42bfc05914047ec9481c97c2
orlandol <orlandol@veribox.net>
2022-05-09 06:23:03 -0400

    Re-purposing Origo into its long term form, as a grammar driven direct compiler generator.

---
+++ clean.bat
@@ -0,0 +1,4 @@
+@echo off
+if exist retg1.c del retg1.c
+if exist retg1.exe del retg1.exe
+if exist origo.exe del origo.exe
---
+++ test.bat
@@ -0,0 +1,4 @@
+@echo off
+if exist retg1.exe del retg1.exe
+if exist retg1.c del retg1.c
+if exist origo.exe if exist retg1.ocg origo retg1.ocg retg1.c
