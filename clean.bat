d26168f4cdd616b83f40a9fb0a0483c70b67754d
orlandol <orlandol@veribox.net>
2022-05-14 12:00:01 -0400

    Added placeholders for import files.
    Bundling the import file with the source file because together they build the compiler executable. In future versions of Origo, the source grammar will build directly into an executable. As a result, future versions of Create Compiler will go from source+import file names, to just an exe file name.

--- clean.bat
+++ clean.bat
@@ -1,6 +1,10 @@
 @echo off
 if exist cgen.c del cgen.c
+if exist cgen.def del cgen.def
 if exist cgen.exe del cgen.exe
+
 if exist retg1.c del retg1.c
+if exist retg1.def del retg1.def
 if exist retg1.exe del retg1.exe
+
 if exist origo.exe del origo.exe
