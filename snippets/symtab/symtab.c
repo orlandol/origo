3dabac8f2053680adea96ad131acfb10738e816d
orlandol <orlandol@veribox.net>
2022-05-23 07:04:16 -0400

    Symbol table prototype code in progress.

--- snippets/symtab/symtab.c
+++ snippets/symtab/symtab.c
@@ -23,6 +23,12 @@ typedef struct SymbolTable {
   struct avl_tree_node node;
 } SymbolTable, Symbol;
 
+Symbol* CreateSymbol( const char* symbolName, unsigned symbolType );
+void FreeSymbol( Symbol** symbolPtr );
+
+SymbolTable* CreateSymbolTable( const char* rootName );
+void ReleaseSymbolTable( SymbolTable** symbolTablePtr );
+
 static int CompareSymbols( const struct avl_tree_node* left,
   const struct avl_tree_node* right );
 
@@ -30,19 +36,83 @@ unsigned DeclareNamespace( SymbolTable* symbolTable, const char* name );
 
 SymbolTable* symtab = NULL;
 
+void DumpTree( SymbolTable* symbolTable ) {
+  Symbol* symbol = NULL;
+
+  avl_tree_for_each_in_postorder(symbol, symbolTable,
+      SymbolTable, node) {
+    printf( "Node[%s] == %u\n", symbol->name, symbol->symtype );
+  }
+}
+
+
 int main( int argc, char** argv ) {
   unsigned result;
 
-  result = DeclareNamespace(symtab, "myprog");
-  if( result ) {
-    printf( "DeclareNamespace error: %u\n", result );
-    exit(1);
-  }
+  symtab = CreateSymbolTable("myprog");
+
+  DumpTree( symtab );
+
+  ReleaseSymbolTable( &symtab );
 
   return 0;
 }
 
 #include "avl_tree.c"
+
+Symbol* CreateSymbol( const char* symbolName, unsigned symbolType ) {
+  Symbol* newSymbol = NULL;
+
+  if( symbolName ) {
+    newSymbol = calloc(1, sizeof(Symbol));
+
+    if( newSymbol ) {
+      newSymbol->name = calloc(1, strlen(symbolName) + 1);
+
+      if( newSymbol->name ) {
+        strcpy( newSymbol->name, symbolName );
+
+        return newSymbol;
+      }
+
+      FreeSymbol( &newSymbol );
+    }
+  }
+
+  return NULL;
+}
+
+void FreeSymbol( Symbol** symbolPtr ) {
+  if( symbolPtr ) {
+    if( (*symbolPtr) ) {
+      if( (*symbolPtr)->name ) {
+        free( (*symbolPtr)->name );
+        (*symbolPtr)->name = NULL;
+      }
+
+      free( (*symbolPtr) );
+      (*symbolPtr) = NULL;
+    }
+  }
+}
+
+SymbolTable* CreateSymbolTable( const char* rootName ) {
+  return (SymbolTable*)calloc(1, sizeof(SymbolTable));
+}
+
+void ReleaseSymbolTable( SymbolTable** symbolTablePtr ) {
+  Symbol* symbol = NULL;
+
+  if( symbolTablePtr ) {
+    avl_tree_for_each_in_postorder(symbol, (*symbolTablePtr),
+        SymbolTable, node) {
+
+      avl_tree_remove( symbolTablePtr, &symbol->node );
+
+      FreeSymbol( &symbol );
+    }
+  }
+}
 
 static int CompareSymbols( const struct avl_tree_node* left,
   const struct avl_tree_node* right ) {
@@ -58,31 +128,15 @@ unsigned DeclareNamespace( SymbolTable* symbolTable, const char* name ) {
 //  Parameter symbolTable is allowed to be NULL
   if( !(name && *name) ) { return 2; }
 
-  Symbol* newNode = NULL;
-
-  newNode = calloc(1, sizeof(Symbol));
+  Symbol* newNode = CreateSymbol(name, symNamespace);
   if( newNode == NULL ) { return 3; }
 
-  newNode->name = calloc(1, strlen(name) + 1);
-  if( newNode->name ) {
-    strcpy( newNode->name, name );
-
-    if( avl_tree_insert(&symbolTable, &newNode->node,
-        CompareSymbols) == NULL ) {
-
-      return 0;
-    }
+  if( avl_tree_insert(&symbolTable, &newNode->node,
+      CompareSymbols) == NULL ) {
+    return 0;
   }
 
-  if( newNode ) {
-    if( newNode->name ) {
-      free( newNode->name );
-      newNode->name = NULL;
-    }
-
-    free( newNode );
-    newNode = NULL;
-  }
+  FreeSymbol( &newNode );
 
   return 3;
 }
