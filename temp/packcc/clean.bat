3cd5fb8bce87ff0b68e194546136ecb038012432
orlandol <orlandol@veribox.net>
2022-07-14 06:47:44 -0400

    Prototype short term reimplementation of Origo as a PEG Retineo G1 to C translator in progress.

--- temp/packcc/clean.bat
+++ temp/packcc/clean.bat
@@ -1,17 +1,17 @@
 @echo off
-if "%1" == "origo" goto SkipDeleteOrigo
+if not "%1" == "" goto DELETEGENERAL
 if exist %1.c del %1.c
+if exist %1.h del %1.h
 if exist %1.def del %1.def
 if exist %1.exe del %1.exe
-:SkipDeleteOrigo
+:DELETEGENERAL
 
-if exist cgen.c del cgen.c
-if exist cgen.def del cgen.def
-if exist cgen.exe del cgen.exe
+if exist test.c del test.c
+if exist test.h del test.h
+if exist test.def del test.def
+if exist test.exe del test.exe
 
-if exist retg1.c del retg1.c
-if exist retg1.def del retg1.def
-if exist retg1.exe del retg1.exe
-
+if exist origo.c del origo.c
+if exist origo.h del origo.h
 if exist origo.def del origo.def
 if exist origo.exe del origo.exe
