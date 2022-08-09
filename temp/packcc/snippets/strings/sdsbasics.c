ce2650b5c4538cb4e1d8c999844d2d6d946ceffa
orlandol <orlandol@veribox.net>
2022-08-09 05:11:23 -0400

    Updated SDS string reference code. Added placeholder for stack reference code.

--- temp/packcc/snippets/strings/sdsbasics.c
+++ temp/packcc/snippets/strings/sdsbasics.c
@@ -2,73 +2,84 @@
 
 #include "sds.h"
 
-/* Mini-ref:
+/* SDS String Mini-Reference
 
-  String Length:
+  SDS string length:
     static inline size_t sdslen(const sds s);
 
-  Create New SDS String And Copy ASCII-Z String:
+  Create new SDS string and copy ASCII-Z string:
     sds sdsnew(const char *init);
 
-  Allocate Empty SDS String:
+  Allocate empty SDS string:
     sds sdsempty(void);
 
-  [Allocate] And Copy SDS String:
+  [Allocate] and copy SDS string:
     sds sdsdup(const sds s);
 
-  Release SDS String:
+  Release SDS string:
     void sdsfree(sds s);
 
-  [Allocate] SDS String And Append ASCII-Z Characters Up To len:
+  [Allocate] SDS string and append ASCII-Z characters up to len:
     sds sdscatlen(sds s, const void *t, size_t len);
 
-  [Allocate] SDS String And Append ASCII-Z String:
+  [Allocate] SDS string and append ASCII-Z string:
     sds sdscat(sds s, const char *t);
 
-  [Allocate] SDS String And Append ASCII-Z String:
+  [Allocate] SDS string and append ASCII-Z string:
     sds sdscatsds(sds s, const sds t);
 
-  [Allocate] SDS String And Copy ASCII-Z Characters Up To len:
+  [Allocate] SDS string and copy ASCII-Z characters up to len:
     sds sdscpylen(sds s, const char *t, size_t len);
 
-  [Allocate] SDS String And Copy ASCII-Z Characters:
+  [Allocate] SDS string and copy ASCII-Z characters:
     sds sdscpy(sds s, const char *t);
 
-  [Allocate] And Append(?) Formatted SDS String Using printf Specifiers:
+  [Allocate] SDS string and append a printf specifier formatted string:
     sds sdscatprintf(sds s, const char *fmt, ...);
 
-  [Allocate] SDS String And Append Formatted
+  [Allocate] SDS string and append subset of printf-like formatted string:
     sds sdscatfmt(sds s, char const *fmt, ...);
 
-  Remove Specified Character Set From Left And Right Of SDS String:
+  Remove specified character set from left and right of the SDS string:
     sds sdstrim(sds s, const char *cset);
 
-  Remove Specified Characters Within Index Range of SDS String:
+  Reduce SDS string to the specified index range (negatives wrap around):
     void sdsrange(sds s, int start, int end);
 
-  Reduce String To An Empty String:
+  Reduce SDS string to an empty string:
     void sdsclear(sds s);
 
-  Compare SDS Strings:
+  Compare SDS strings:
     int sdscmp(const sds s1, const sds s2);
 
+  Allocate an array of SDS strings according to specified separators:
     sds *sdssplitlen(const char *s, int len,
       const char *sep, int seplen, int *count);
+
+  Release memory used by SDS string array:
     void sdsfreesplitres(sds *tokens, int count);
+
+  Set SDS string to all lower case:
     void sdstolower(sds s);
+
+  Set SDS string to all upper case:
     void sdstoupper(sds s);
+
+  Directly allocate an SDS string converted from a numeric value:
     sds sdsfromlonglong(long long value);
+
+  Replace specified characters in the SDS string to the replacement set:
     sds sdsmapchars(sds s, const char *from, const char *to, size_t setlen);
-    sds sdsjoin(char **argv, int argc, char *sep);
-    sds sdsjoinsds(sds *argv, int argc, const char *sep, size_t seplen);
  */
 
 int main( int argc, char** argv ) {
   int identCount = 0;
-  sds* qualifiedIdent = sdssplitlen("foo.bar.blah", 12, ".", 1, &identCount);
+  sds parsedIdent = sdsnew("  foo .bar. blah ");
+  sds* qualifiedIdent = sdssplitlen(parsedIdent, sdslen(parsedIdent), ".", 1, &identCount);
 
   int index = 0;
   while( index < identCount ) {
+    qualifiedIdent[index] = sdstrim(qualifiedIdent[index], " ");
     printf( "[%s]\n", qualifiedIdent[index++] );
   }
 
