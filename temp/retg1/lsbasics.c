be97007f9b82b988b5c455140ca7fad88c856232
orlandol <orlandol@veribox.net>
2022-10-27 08:36:46 -0400

    Unsorted list prototype in progress.

--- temp/retg1/lsbasics.c
+++ temp/retg1/lsbasics.c
@@ -25,14 +25,15 @@ unsigned ReleaseListItems( LIST_TYPENAME* list,
 unsigned GrowList( LIST_TYPENAME* list );
 unsigned CompactList( LIST_TYPENAME* list );
 
-unsigned InsertItem( LIST_TYPENAME* list, LISTITEM_TYPENAME fromItem );
-unsigned InsertItemAt( LIST_TYPENAME* list, unsigned index, LISTITEM_TYPENAME fromItem );
+unsigned InsertItem( LIST_TYPENAME* list, LISTITEM_TYPENAME sourceItem );
+unsigned InsertItemAt( LIST_TYPENAME* list, unsigned index, LISTITEM_TYPENAME sourceItem );
 
-unsigned RemoveItemAt( LIST_TYPENAME* list, unsigned index, LISTITEM_TYPENAME* toItem );
+unsigned RemoveItemAt( LIST_TYPENAME* list, unsigned index, LISTITEM_TYPENAME* destItem );
 
-unsigned ListItem( LIST_TYPENAME* list, unsigned atIndex, LISTITEM_TYPENAME* toItem );
+unsigned ReadItem( LIST_TYPENAME* list, unsigned atIndex, LISTITEM_TYPENAME* destItem );
+unsigned WriteItem( LIST_TYPENAME* list, unsigned atIndex, const LISTITEM_TYPENAME* sourceItem );
 
-unsigned FindItem( LIST_TYPENAME* list, unsigned byValue, LISTITEM_TYPENAME* toItem );
+unsigned FindItem( LIST_TYPENAME* list, unsigned byValue, LISTITEM_TYPENAME* destItem );
 
 /*
  *  Main program
@@ -65,30 +66,78 @@ unsigned ReleaseListItems( LIST_TYPENAME* list,
   return 3;
 }
 
+/*
+#define IMPLEMENT_GROWSTACK( STACKSLOT_TYPENAME, STACK_TYPENAME, FUNCNAME )\
+unsigned FUNCNAME( STACK_TYPENAME* stack ) {\
+  STACKSLOT_TYPENAME* newSlot = NULL;\
+  unsigned newTop;\
+  unsigned newBottom;\
+  \
+  if( stack == NULL ) { return 1; }\
+  \
+  if( (((unsigned)-1) - stack->top) < STACK_GROWTH_INCREMENT ) {\
+    return 2;\
+  }\
+  \
+  newTop = (stack->top & (~STACK_GROWTH_MASK)) + STACK_GROWTH_INCREMENT;\
+  newBottom = newTop - (stack->top - stack->bottom);\
+  \
+  if( stack->bottom == 0 ) {\
+    newSlot = realloc(stack->slot, newTop * sizeof(STACKSLOT_TYPENAME));\
+    if( newSlot == NULL ) {\
+      return 3;\
+    }\
+    \
+    if( stack->slot ) {\
+      memmove( &newSlot[newBottom], &newSlot[stack->bottom],\
+        stack->bottom * sizeof(STACKSLOT_TYPENAME) );\
+      memset( newSlot, 0, stack->bottom * sizeof(STACKSLOT_TYPENAME) );\
+    }\
+    \
+    stack->slot = newSlot;\
+    stack->top = newTop;\
+    stack->bottom = newBottom;\
+  }\
+  \
+  return 0;\
+}
+*/
+
 unsigned GrowList( LIST_TYPENAME* list ) {
-  return 2;
+  LISTITEM_TYPENAME* newItem = NULL;
+  unsigned newReserveCount = 0;
+
+  if( list == NULL ) { return 1; }
+
+  if( (((unsigned)-1) - list->reserveCount) < LIST_GROWTH_INCREMENT ) {
+    return 2;
+  }
+
+  newReserveCount = (list->reserveCount & (~LIST_GROWTH_MASK)) + LIST_GROWTH_INCREMENT;
+
+  return 3;
 }
 
 unsigned CompactList( LIST_TYPENAME* list ) {
   return 2;
 }
 
-unsigned InsertItem( LIST_TYPENAME* list, LISTITEM_TYPENAME fromItem ) {
+unsigned InsertItem( LIST_TYPENAME* list, LISTITEM_TYPENAME sourceItem ) {
   return 3;
 }
 
-unsigned InsertItemAt( LIST_TYPENAME* list, unsigned index, LISTITEM_TYPENAME fromItem ) {
+unsigned InsertItemAt( LIST_TYPENAME* list, unsigned index, LISTITEM_TYPENAME sourceItem ) {
   return 4;
 }
 
-unsigned RemoveItemAt( LIST_TYPENAME* list, unsigned index, LISTITEM_TYPENAME* toItem ) {
+unsigned RemoveItemAt( LIST_TYPENAME* list, unsigned index, LISTITEM_TYPENAME* destItem ) {
   return 4;
 }
 
-unsigned ListItem( LIST_TYPENAME* list, unsigned atIndex, LISTITEM_TYPENAME* toItem ) {
+unsigned ListItem( LIST_TYPENAME* list, unsigned atIndex, LISTITEM_TYPENAME* destItem ) {
   return 4;
 }
 
-unsigned FindItem( LIST_TYPENAME* list, unsigned byValue, LISTITEM_TYPENAME* toItem ) {
+unsigned FindItem( LIST_TYPENAME* list, unsigned byValue, LISTITEM_TYPENAME* destItem ) {
   return 4;
 }
